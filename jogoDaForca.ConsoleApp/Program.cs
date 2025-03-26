6bf96f155d2d2038ecc4cc4f5ac89bb4c9206f35
alexandreSouza31 <alexandre31_m@hotmail.com>
2025-03-26 00:30:51 -0300

    adicionará a lógica de erros do usuário

--- jogoDaForca.ConsoleApp/Program.cs
+++ jogoDaForca.ConsoleApp/Program.cs
@@ -7,15 +7,14 @@
             while (true) {
                 string palavraSecreta = "MELANCIA";
 
-
                 char[] letrasEncontradas = new char[palavraSecreta.Length];
 
                 for (int caractere = 0; caractere < letrasEncontradas.Length; caractere++)
                 {
                     letrasEncontradas[caractere] = '_';
-
                 }
 
+                int qtdErros = 0;
                 bool jogadorEnforcou = false;
                 bool jogadorAcertou = false;
 
@@ -23,14 +22,25 @@
                 do
                 {
                     string dicaPalavra = String.Join(" ", letrasEncontradas);
-
+                    jogadorEnforcou = qtdErros > 5;
 
                     Console.Clear();
                     Console.WriteLine("----------------------------------------------");
                     Console.WriteLine("              Jogo da Forca");
                     Console.WriteLine("----------------------------------------------");
+                    Console.WriteLine(" ___________        ");
+                    Console.WriteLine(" |/        |        ");
+                    Console.WriteLine(" |                  ");
+                    Console.WriteLine(" |                  ");
+                    Console.WriteLine(" |                  ");
+                    Console.WriteLine(" |                  ");
+                    Console.WriteLine(" |                  ");
+                    Console.WriteLine(" |                  ");
+                    Console.WriteLine("_|____              ");
+
+                    Console.WriteLine("\nErros do jogador: " + qtdErros);
                     Console.WriteLine("----------------------------------------------");
-                    Console.WriteLine("Palavra escolhida: " + String.Join("", letrasEncontradas));
+                    Console.WriteLine("Palavra escolhida: " + String.Join(" ", letrasEncontradas));
                     Console.Write("Digite uma letra: ");
                     char chute = Console.ReadLine().ToUpper()[0];
                     Console.WriteLine(chute);
@@ -49,8 +59,48 @@
                         }
                     }
 
+                    if (letraFoiEncontrada == false && !jogadorAcertou)
+                    {
+                        qtdErros++;
+                    }
 
-                    dicaPalavra= String.Join(" ", letrasEncontradas);
+                    dicaPalavra = String.Join(" ", letrasEncontradas);
+
+                    jogadorAcertou = new string(letrasEncontradas) == palavraSecreta;
+
+                    jogadorEnforcou = qtdErros >= 5;
+
+                    if (jogadorAcertou)
+                    {
+                        Console.WriteLine("Acertou! A palavra era "+palavraSecreta);
+                        Console.ReadLine();
+                        continue;
+                    }
+                    else if (jogadorEnforcou)
+                    {
+                        Console.WriteLine("Errou! A palavra era "+palavraSecreta);
+                        Console.WriteLine("Tente Novamente!");
+                        Console.ReadLine();
+                        Console.Clear();
+                        Console.WriteLine("----------------------------------------------");
+                        Console.WriteLine("              Jogo da Forca");
+                        Console.WriteLine("----------------------------------------------");
+                        Console.WriteLine(" ___________        ");
+                        Console.WriteLine(" |/        |        ");
+                        Console.WriteLine(" |                  ");
+                        Console.WriteLine(" |                  ");
+                        Console.WriteLine(" |                  ");
+                        Console.WriteLine(" |                  ");
+                        Console.WriteLine(" |                  ");
+                        Console.WriteLine(" |                  ");
+                        Console.WriteLine("_|____              ");
+
+                        Console.WriteLine("\nErros do jogador: " + qtdErros);
+                        Console.WriteLine("----------------------------------------------");
+                        Console.WriteLine("Palavra escolhida: " + String.Join(" ", palavraSecreta));
+                        Console.ReadLine();
+
+                    }
 
                 }
                 while (jogadorAcertou==false && jogadorEnforcou==false);
