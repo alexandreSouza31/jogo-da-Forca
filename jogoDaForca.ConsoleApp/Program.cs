63900467970f8b98d0309b1d4d1d09b4356d8d79
alexandreSouza31 <alexandre31_m@hotmail.com>
2025-03-25 23:44:09 -0300

    criará a estrutura inicial do jogo

--- jogoDaForca.ConsoleApp/Program.cs
+++ jogoDaForca.ConsoleApp/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace jogoDaForca.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) {
+                string palavraSecreta = "MELANCIA";
+
+
+                char[] letrasEncontradas = new char[palavraSecreta.Length];
+
+                for (int caractere = 0; caractere < letrasEncontradas.Length; caractere++)
+                {
+                    letrasEncontradas[caractere] = '_';
+
+                }
+
+                bool jogadorEnforcou = false;
+                bool jogadorAcertou = false;
+
+
+                do
+                {
+                    string dicaPalavra = String.Join(" ", letrasEncontradas);
+
+
+                    Console.Clear();
+                    Console.WriteLine("----------------------------------------------");
+                    Console.WriteLine("              Jogo da Forca");
+                    Console.WriteLine("----------------------------------------------");
+                    Console.WriteLine("----------------------------------------------");
+                    Console.WriteLine("Palavra escolhida: " + String.Join("", letrasEncontradas));
+                    Console.Write("Digite uma letra: ");
+                    char chute = Console.ReadLine().ToUpper()[0];
+                    Console.WriteLine(chute);
+
+
+                    bool letraFoiEncontrada=false;
+
+                    for(int contador = 0; contador < palavraSecreta.Length; contador++)
+                    {
+                        char letraAtual = palavraSecreta[contador];
+
+                        if (chute == letraAtual)
+                        {
+                            letrasEncontradas[contador] = letraAtual;
+                            letraFoiEncontrada = true;
+                        }
+                    }
+
+
+                    dicaPalavra= String.Join(" ", letrasEncontradas);
+
+                }
+                while (jogadorAcertou==false && jogadorEnforcou==false);
+
+            }
+        }
+    }
+}
